8af1347c379981316544d3b9b78860902a6e8be0
Eric Sommer <ericsomm@gmx.de>
2019-01-07 14:40:48 +0100

    limits are set endogenously

--- makegraph.py
+++ makegraph.py
@@ -7,18 +7,19 @@
 
 import matplotlib.pyplot as plt
 
-def makeplot(data,
-             xlab='',
-             ylab='',
-             title='',
-             fs=14,
-             color='bw',
-             showlegend=True,
-             figs=(10, 5),
-             style='seaborn-whitegrid',
-             plottype='line',
-             **kwargs
-             ):
+
+def make_plot(data,
+              xlab='',
+              ylab='',
+              title='',
+              fs=14,
+              color='bw',
+              showlegend=True,
+              figs=(5*1.618, 5),
+              style='seaborn-whitegrid',
+              plottype='line',
+              **kwargs
+              ):
     '''
     Wrapper for matplotlib plots, avoiding the hassle of specifying the look each and every time
 
@@ -34,17 +35,17 @@ def makeplot(data,
     lines = {'bw': ['k-', 'k--', 'k:', 'k-.', 'k-', 'k--', 'k:', 'k-.'],
              'color': ['b-', 'r-', 'g-', 'c-', 'm-', 'b-', 'r-', 'g-', 'c-']
              }
-    colors = {'bw': ['black' for i in (range(0, 9))],
-             'color': ['navy',
-                       'maroon',
-                       'forestgreen',
-                       'darkmagenta',
-                       'lightskyblue',
-                       'darkorange']
+    colors = {'bw': ['black' for i in range(0, 9)],
+              'color': ['navy',
+                        'maroon',
+                        'forestgreen',
+                        'darkmagenta',
+                        'lightskyblue',
+                        'darkorange']
              }
 
     markers = {'bw': ['.', ',', 'o', 's', 'D', 'v', '+'],
-               'color': ['o' for i in (range(0, 9))]
+               'color': ['o' for i in range(0, 9)]
                }
 
     fillstyles = {'bw': 'none',
@@ -62,12 +63,11 @@ def makeplot(data,
     xlim_high = kwargs.get('xlim_high', data[firstkey][0].max() * 1.1)
     ylim_low = kwargs.get('ylim_low', data[firstkey][1].min() * 1.1)
     ylim_high = kwargs.get('ylim_high', data[firstkey][1].max() * 1.1)
-    xaxis_grid = kwargs.get('yaxis_grid', False)
-    yaxis_grid = kwargs.get('yaxis_grid', True)
-    subplot = kwargs.get('subplot', False)
+    xaxis_grid = kwargs.get('yaxis_grid', True)
+    yaxis_grid = kwargs.get('yaxis_grid', False)
 
     lw = kwargs.get('lw', 2)
-    # Switch off vertical grid lines?
+    # Switch off grid lines?
     plt.gca().xaxis.grid(xaxis_grid)
     plt.gca().yaxis.grid(yaxis_grid)
 
@@ -80,8 +80,15 @@ def makeplot(data,
                    labelsize=fs-2)
 
     # Set limits
-    ax.set_ylim(ylim_low, ylim_high)
-    ax.set_xlim(xlim_low, xlim_high)
+    try:
+        ax.set_ylim(ylim_low, ylim_high)
+    except ValueError:
+        raise Exception('Invalid limits for y axis. You may need to specify them yourself.')
+    try:
+        ax.set_xlim(xlim_low, xlim_high)
+    except ValueError:
+        raise Exception('Invalid limits for x axis. You may need to specify them yourself.')
+
     # Title
     ax.set_title(title, size=fs+2)
     # X Axis Label
@@ -89,28 +96,29 @@ def makeplot(data,
                   size=fs)
     # Y axis label
     ax.set_ylabel(ylab,
-		           size=fs)
+		          size=fs)
     # legend
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.2,
                      1.05 * box.width, 1.05 * (box.height * 0.85)])
     count = 0
+    axlist = []
     for l, plotdata in data.items():
         if plottype == 'line':
-            ax = ax.plot(plotdata[0],
-                         plotdata[1],
-                         lines[color][count],
-                         c=colors[color][count],
-                         label=l,
-                         linewidth=lw
-                         )
+            ax.plot(plotdata[0],
+                    plotdata[1],
+                    lines[color][count],
+                    c=colors[color][count],
+                    label=l,
+                    linewidth=lw
+                    )
         if plottype == 'scatter':
-            ax = ax.scatter(plotdata[0],
-                            plotdata[1],
-                            marker=markers[color][count],
-                            c=colors[color][count],
-                            # fillstyle=fillstyles[color],
-                            label=l
+            ax.scatter(plotdata[0],
+                       plotdata[1],
+                       marker=markers[color][count],
+                       c=colors[color][count],
+                       # fillstyle=fillstyles[color],
+                       label=l
                     )
         count += 1
 
@@ -119,8 +127,24 @@ def makeplot(data,
                   fontsize=fs,
                   bbox_to_anchor=(0.48, -0.15),
                   ncol=2)
-    # Return the Plot
-    if subplot:
-        return ax
-    else:
-        return plt
+
+
+    return fig, ax
+
+def combine_subplots(axlist,
+                     nrows,
+                     ncols,
+                     figs=(5*1.618, 5)
+                     ):
+    # plt.clf()
+    fig2 = plt.figure(figsize=figs)
+
+    for a in axlist:
+        ix = axlist.index(a) + 1
+        print(ix)
+        a = fig2.add_subplot(nrows,
+                            ncols,
+                            ix)
+
+
+    return plt
